37dcbe8e38701c1f8977caf21ab9e1854e73d372
Tiannabm <143770133+Tiannabm@users.noreply.github.com>
2023-11-03 21:36:05 -0400

    Add files via upload

---
+++ src/GuessingGame.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GuessingGame() {
+
+
+    
+    return (
+
+        <p>I am thinking of a number between 1 and 100. Guess the Lucky Number!</p>
+    )
+}
+
+export default GuessingGame;
